c4ebb64151ea9a1469b45c1d5f87dd971ecc117a
bc-chou <98440491+bc-chou@users.noreply.github.com>
2022-11-13 00:58:05 +0800

    fixed price scheme for 3 age groups, some movie initialisation.

--- src/Control/PriceScheme_Controller.java
+++ src/Control/PriceScheme_Controller.java
@@ -79,46 +79,66 @@ public class PriceScheme_Controller {
 
         Float price = (float) 0;
         String key = "";
-        if(age < 21){
-            key = String.format("%s CINEMA && %s MOVIE && AGE < 21", classOfCinema.name(), typeOfMovie.name());
+        if(age < 18){
+            key = String.format("%s CINEMA && %s MOVIE && AGE < 18", classOfCinema.name(), typeOfMovie.name());
             price  = priceMap.get(key);
         }
-        else{
-            key = String.format("%s CINEMA && %s MOVIE && AGE > 21", classOfCinema.name(), typeOfMovie.name());
+        
+        else if (age > 60){
+            key = String.format("%s CINEMA && %s MOVIE && AGE > 60", classOfCinema.name(), typeOfMovie.name());
             price  = priceMap.get(key);
         }
+         else{
+            key = String.format("%s CINEMA && %s MOVIE && (AGE >= 18 && AGE < 60) ", classOfCinema.name(), typeOfMovie.name());
+            price  = priceMap.get(key);
+        } 
 
         if(Holiday_Controller.isHoliday(showTime)){
             price += priceMap.get("HOLIDAY INCREASE PRICE");
-        }
+        } 
 
         return price;
     }
 
     public static void main(String args[]){
         HashMap<String, Float> priceMap = new HashMap<String, Float>();
-        priceMap.put("NORMAL CINEMA && DIGITAL MOVIE && AGE < 21", (float) 10);
-        priceMap.put("NORMAL CINEMA && DIGITAL MOVIE && AGE > 21", (float) 12.5);
-        priceMap.put("NORMAL CINEMA && BLOCKBUSTER MOVIE && AGE < 21", (float) 12.5);
-        priceMap.put("NORMAL CINEMA && BLOCKBUSTER MOVIE && AGE > 21", (float) 15);
-        priceMap.put("NORMAL CINEMA && _3D MOVIE && AGE < 21", (float) 15);
-        priceMap.put("NORMAL CINEMA && _3D MOVIE && AGE > 21", (float) 17.5);
+        priceMap.put("NORMAL CINEMA && DIGITAL MOVIE && AGE < 18", (float) 7);
+        priceMap.put("NORMAL CINEMA && DIGITAL MOVIE && (AGE >= 18 && AGE < 60)", (float) 10);
+        priceMap.put("NORMAL CINEMA && DIGITAL MOVIE && AGE > 60", (float) 5);
+
+        priceMap.put("NORMAL CINEMA && BLOCKBUSTER MOVIE && AGE < 18", (float) 8);
+        priceMap.put("NORMAL CINEMA && BLOCKBUSTER MOVIE && (AGE >= 18 && AGE < 60)", (float) 11);
+        priceMap.put("NORMAL CINEMA && BLOCKBUSTER MOVIE && AGE > 60", (float) 6);
+
+        priceMap.put("NORMAL CINEMA && _3D MOVIE && AGE < 18", (float) 10);
+        priceMap.put("NORMAL CINEMA && _3D MOVIE && (AGE >= 18 && AGE < 60)", (float) 13);
+        priceMap.put("NORMAL CINEMA && _3D MOVIE && AGE > 60", (float) 8);
         
-        priceMap.put("MAX CINEMA && DIGITAL MOVIE && AGE < 21", (float) 12.5);
-        priceMap.put("MAX CINEMA && DIGITAL MOVIE && AGE > 21", (float) 15);
-        priceMap.put("MAX CINEMA && BLOCKBUSTER MOVIE && AGE < 21", (float) 15);
-        priceMap.put("MAX CINEMA && BLOCKBUSTER MOVIE && AGE > 21", (float) 17.5);
-        priceMap.put("MAX CINEMA && _3D MOVIE && AGE < 21", (float) 17.5);
-        priceMap.put("MAX CINEMA && _3D MOVIE && AGE > 21", (float) 20);
+        priceMap.put("MAX CINEMA && DIGITAL MOVIE && AGE < 18", (float) 12);
+        priceMap.put("MAX CINEMA && DIGITAL MOVIE && (AGE >= 18 && AGE < 60)", (float) 15);
+        priceMap.put("MAX CINEMA && DIGITAL MOVIE && AGE > 60", (float) 10);
 
-        priceMap.put("GOLD CINEMA && DIGITAL MOVIE && AGE < 21", (float) 15);
-        priceMap.put("GOLD CINEMA && DIGITAL MOVIE && AGE > 21", (float) 17.5);
-        priceMap.put("GOLD CINEMA && BLOCKBUSTER MOVIE && AGE < 21", (float) 17.5);
-        priceMap.put("GOLD CINEMA && BLOCKBUSTER MOVIE && AGE > 21", (float) 20);
-        priceMap.put("GOLD CINEMA && _3D MOVIE && AGE < 21", (float) 20);
-        priceMap.put("GOLD CINEMA && _3D MOVIE && AGE > 21", (float) 22.5);
+        priceMap.put("MAX CINEMA && BLOCKBUSTER MOVIE && AGE < 18", (float) 13);
+        priceMap.put("MAX CINEMA && BLOCKBUSTER MOVIE && (AGE >= 18 && AGE < 60)", (float) 16);
+        priceMap.put("MAX CINEMA && BLOCKBUSTER MOVIE && AGE > 60", (float) 11);
 
-        priceMap.put("HOLIDAY INCREASE PRICE", (float) 5);
+        priceMap.put("MAX CINEMA && _3D MOVIE && AGE < 18", (float) 15);
+        priceMap.put("MAX CINEMA && _3D MOVIE && (AGE >= 18 && AGE < 60)", (float) 16);
+        priceMap.put("MAX CINEMA && _3D MOVIE && AGE > 60", (float) 11);
+
+        priceMap.put("GOLD CINEMA && DIGITAL MOVIE && AGE < 18", (float) 17);
+        priceMap.put("GOLD CINEMA && DIGITAL MOVIE && (AGE >= 18 && AGE < 60)", (float) 20);
+        priceMap.put("GOLD CINEMA && DIGITAL MOVIE && AGE > 60", (float) 15);
+
+        priceMap.put("GOLD CINEMA && BLOCKBUSTER MOVIE && AGE < 18", (float) 18);
+        priceMap.put("GOLD CINEMA && BLOCKBUSTER MOVIE && (AGE >= 18 && AGE < 60)", (float) 21);
+        priceMap.put("GOLD CINEMA && BLOCKBUSTER MOVIE && AGE > 60", (float) 16);
+
+        priceMap.put("GOLD CINEMA && _3D MOVIE && AGE < 18", (float) 20);
+        priceMap.put("GOLD CINEMA && _3D MOVIE && (AGE >= 18 && AGE < 60)", (float) 23);
+        priceMap.put("GOLD CINEMA && _3D MOVIE && AGE > 60", (float) 18);
+
+        priceMap.put("HOLIDAY INCREASE PRICE", (float) 3);
 
         ArrayList<HashMap<String, Float>> priceList = new ArrayList<HashMap<String, Float>>();
         priceList.add(priceMap);
